dc44da414849f75437dc4863519e9ebf66aa4a6d
hamodte3 <hamodte3@gmail.com>
2026-09-19 17:43:15 +0300

    Update depinfo.sh

--- buildscripts/include/depinfo.sh
+++ buildscripts/include/depinfo.sh
@@ -1,52 +1,35 @@
 #!/bin/bash -e
 
-## Dependency versions
-# Make sure to keep v_ndk and v_ndk_n in sync, both are listed on the NDK download page
+cd "$( dirname "${BASH_SOURCE[0]}" )/.."
+. ./include/depinfo.sh
 
-v_sdk=11076708_latest
-v_ndk=r30
-v_ndk_n=30.0.16248370
-v_sdk_platform=36
-v_sdk_build_tools=36.0.0
+mkdir -p deps
+cd deps
 
-# تم الإبقاء فقط على حزم التشفير والشبكة الضرورية لروابط البث (HTTPS)
-v_mbedtls=3.6.7
-v_curl=8.21.0
+# 1. تحميل mbedtls
+if [ ! -d mbedtls ]; then
+    echo "==> Fetching mbedtls"
+    wget -qO- https://github.com/Mbed-TLS/mbedtls/releases/download/mbedtls-${v_mbedtls}/mbedtls-${v_mbedtls}.tar.bz2 | tar -xjf -
+    mv mbedtls-${v_mbedtls} mbedtls
+fi
 
+# 2. استنساخ FFmpeg
+if [ ! -d ffmpeg ]; then
+    echo "==> Cloning ffmpeg"
+    git clone --depth 1 --branch ${v_ci_ffmpeg} https://github.com/FFmpeg/FFmpeg.git ffmpeg
+fi
 
-## Dependency tree (Ultra-Lean Architecture)
+# 3. استنساخ libplacebo
+if [ ! -d libplacebo ]; then
+    echo "==> Cloning libplacebo"
+    git clone --depth 1 --recursive https://code.videolan.org/videolan/libplacebo.git libplacebo
+fi
 
-dep_mbedtls=()
-dep_dav1d=()
-dep_libxml2=()
+# 4. تحميل curl
+if [ ! -d curl ]; then
+    echo "==> Fetching curl"
+    wget -qO- https://curl.se/download/curl-${v_curl}.tar.xz | tar -xJf -
+    mv curl-${v_curl} curl
+fi
 
-# FFmpeg يعتمد فقط على mbedtls لفتح روابط البث المشفرة (HTTPS/TLS)
-dep_ffmpeg=(mbedtls)
-
-# تم تصفير وتفريغ منظومة الترجمة والخطوط بالكامل
-dep_freetype2=()
-dep_fontconfig=()
-dep_fribidi=()
-dep_harfbuzz=()
-dep_unibreak=()
-dep_libass=()
-
-# تم إيقاف محركات السكربتات
-dep_lua=()
-
-# محرك الرندر الأساسي والشبكة
-dep_libplacebo=()
-dep_curl=(mbedtls)
-
-# مشغل MPV مصفى: فيديو + صوت + رندر + شبكة فقط
-dep_mpv=(ffmpeg libplacebo curl)
-dep_mpv_android=(mpv)
-
-
-## for CI workflow
-
-# pinned ffmpeg revision
-v_ci_ffmpeg=n9.0
-
-# بصمة الكاش الجديدة المصفاة من كل مكتبات الترجمة الميتة
-ci_tarball="prefix-n${v_ndk}-m${v_mbedtls}-c${v_curl}-ff${v_ci_ffmpeg}-arm64-ultralean.tgz"
+echo "==> Dependencies downloaded successfully (Ultra-Lean)."
