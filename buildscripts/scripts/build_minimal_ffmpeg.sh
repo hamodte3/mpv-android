9e8f86cb579afeca76aeb3c32b38005c58a92de5
hamodte3 <hamodte3@gmail.com>
2026-09-20 17:04:29 +0300

    Update build_minimal_ffmpeg.sh

--- buildscripts/scripts/build_minimal_ffmpeg.sh
+++ buildscripts/scripts/build_minimal_ffmpeg.sh
@@ -7,11 +7,26 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PREFIX="${SCRIPT_DIR}/prefix"
 BUILD_DIR="${SCRIPT_DIR}/build/ffmpeg"
 FFMPEG_SRC="${SCRIPT_DIR}/deps/ffmpeg"
+[ -d "${FFMPEG_SRC}" ] || FFMPEG_SRC="${SCRIPT_DIR}/../deps/ffmpeg"
 
 mkdir -p "${BUILD_DIR}" "${PREFIX}"
 cd "${BUILD_DIR}"
 
-# تصدير مسار pkgconfig ليتعرف configure على mbedtls تلقائياً
+# 1. توليد ملف mbedtls.pc يدوياً ليتعرف عليه FFmpeg بنسبة 100%
+mkdir -p "${PREFIX}/lib/pkgconfig"
+cat << EOF > "${PREFIX}/lib/pkgconfig/mbedtls.pc"
+prefix=${PREFIX}
+exec_prefix=\${prefix}
+libdir=\${exec_prefix}/lib
+includedir=\${prefix}/include
+
+Name: mbedtls
+Description: mbedtls crypto and ssl
+Version: 3.6.0
+Libs: -L\${libdir} -lmbedtls -lmbedx509 -lmbedcrypto
+Cflags: -I\${includedir}
+EOF
+
 export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH:-}"
 
 SAFE_CFLAGS="${EXTRA_CFLAGS:-} -I${PREFIX}/include -fvisibility=default"
@@ -30,7 +45,6 @@ FFMPEG_MINIMAL_FLAGS=(
   --strip=llvm-strip
   --prefix="${PREFIX}"
   --pkg-config=pkg-config
-  --pkg-config-flags="--static"
   --enable-version3
   --enable-mbedtls
   --disable-static
@@ -39,7 +53,6 @@ FFMPEG_MINIMAL_FLAGS=(
   --disable-doc
   --disable-programs
   --disable-encoders
-  --disable-muxers
   --disable-devices
   --disable-filters
   --disable-iconv
@@ -63,7 +76,8 @@ FFMPEG_MINIMAL_FLAGS=(
 "${FFMPEG_SRC}/configure" "${FFMPEG_MINIMAL_FLAGS[@]}" \
   --extra-cflags="${SAFE_CFLAGS}" \
   --extra-ldflags="${SAFE_LDFLAGS}" \
-  --extra-libs="-lmbedtls -lmbedx509 -lmbedcrypto -pthread"
-  
+  --extra-libs="-lmbedtls -lmbedx509 -lmbedcrypto"
+
 make -j"$(nproc)"
 make install
+echo "==> Minimal FFmpeg installed successfully."
